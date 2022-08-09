f13689eac03d6403533094f81a35bab769bcc1e3
Tima <timachulkov1@gmail.com>
2022-08-09 18:18:30 +0500

    Добавьте файлы проекта.

---
+++ JsonSerialization/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSerialization
+{
+	internal class Person
+	{
+		public Int32 Id { get; set; }
+		public Guid TransportId { get; set; }
+		public String FirstName { get; set; }
+		public String LastName { get; set; }
+		public Int32 SequenceId { get; set; }
+		public String[] CreditCardNumbers { get; set; }
+		public Int32 Age { get; set; }
+		public String[] Phones { get; set; }
+		public Int64 BirthDate { get; set; }
+		public Double Salary { get; set; }
+		public Boolean IsMarred { get; set; }
+		public Gender Gender { get; set; }
+		public Child[] Children { get; set; }
+	}
+	class Child
+	{
+		public Int32 Id { get; set; }
+		public String FirstName { get; set; }
+		public String LastName { get; set; }
+		public Int64 BirthDate { get; set; }
+		public Gender Gender { get; set; }
+	}
+	enum Gender
+	{
+		Male,
+		Female
+	}
+	//internal class PersonGenerator : IGenerate
+	//{
+	//	readonly int personAmount = 0;
+	//	internal PersonGenerator(int count)
+ //       {
+	//		personAmount = count;
+	//	}
+ //       public void Generate(Person person)
+ //       {
+			
+	//	}
+ //   }
+ //   class ChildGenerator : IGenerate
+	//{
+ //       public void Generate(Child child)
+ //       {
+           
+ //       }
+ //   }
+ //   internal class JsonSerialaze
+	//{
+	//	List<T> valuesSerialized;
+	//	internal void JsonSerialaze (List<T> values)
+	//	{
+	//		valuesSerialized = values;
+	//	}
+	//}
+}
---
+++ JsonSerialization/PersonCreation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSerialization
+{
+    internal class PersonCreation : IGenerate
+    {
+        Random random = new Random();
+
+        public void Generate(Person person)
+        {
+            var Id = Guid.NewGuid();
+            return new Person()
+            {
+
+            };
+        }
+        private IEnumerable<String> GenerateCreditCards(int creditCardsNum)
+        {
+            var stringBuilder = new StringBuilder(16);
+            for (int i = 0; i < creditCardsNum; i++)
+            {
+                stringBuilder.Clear();
+                for (var j = 0; j < 16; j++)
+                {
+                    stringBuilder.Append(random.Next(1, 9));
+                }
+                yield return stringBuilder.ToString();
+            }
+        }
+        private IEnumerable<String> GeneratePhones(int phoneNumbers)
+        {
+            var stringBuilder = new StringBuilder(11);
+            for (int i = 0; i < phoneNumbers; i++)
+            {
+                stringBuilder.Clear();
+                stringBuilder.Append("+");
+                for(var j = 0; j < 11; j++)
+                {
+                    stringBuilder.Append(random.Next(1, 9));
+                }
+                yield return stringBuilder.ToString();
+            }
+        }
+        private IEnumerable<Child> GenerateChildren(int childrenNum)
+        {
+            for (var i = 0; i < childrenNum; i++)
+            {
+                var childId = Guid.NewGuid();
+                yield return new Child()
+                {
+                    FirstName =$"{childId}",
+                    LastName = $"{childId}",
+                    BirthDate = (DateTime.UtcNow - TimeSpan.FromDays(random.Next(1 * 365, 18 * 365))).ToBinary(),
+                    Gender = random.NextDouble() > 0.5 ? Gender.Female : Gender.Male
+                };
+            }
+        }
+    }
+}
